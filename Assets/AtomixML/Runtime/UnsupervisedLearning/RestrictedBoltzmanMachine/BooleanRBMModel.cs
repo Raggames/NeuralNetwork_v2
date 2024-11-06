0d2f2f2f26361118602c578b07fb994bc0b84432
Raggames <enzoboudart@gmail.com>
2024-11-06 23:11:22 +0100

    w boltzman

--- Assets/AtomixML/Runtime/UnsupervisedLearning/RestrictedBoltzmanMachine/BooleanRBMModel.cs
+++ Assets/AtomixML/Runtime/UnsupervisedLearning/RestrictedBoltzmanMachine/BooleanRBMModel.cs
@@ -1,4 +1,5 @@
 using Atom.MachineLearning.Core;
+using Atom.MachineLearning.Core.Maths;
 using Atom.MachineLearning.Unsupervised.AutoEncoder;
 using System;
 using UnityEngine;
@@ -19,27 +20,72 @@ namespace Atom.MachineLearning.Unsupervised.BoltzmanMachine
         [SerializeField, LearnedParameter] private double[] _hiddenBias;
         private System.Random _random;
 
-        public BooleanRBMModel(string modelName, int visibleUnits, int hiddenUnits)
+        public BooleanRBMModel(int seed, string modelName, int visibleUnits, int hiddenUnits)
         {
             ModelName = modelName;
 
             this._weights = new double[hiddenUnits, visibleUnits];
             this._visibleBias = new double[visibleUnits];
             this._hiddenBias = new double[hiddenUnits];
-            this._random = new System.Random();
+            this._random = new System.Random(seed);
 
+            for (int i = 0; i < _weights.GetLength(0); ++i)
+            {
+                for (int j = 0; j < _weights.GetLength(1); ++j)
+                {
+                    _weights[i, j] = _random.NextDouble() * 2 - 1;
+                }
+            }
         }
+
+        /*public NVector GibbsSample(int[] initialVisible)
+        {
+            int[] hidden = SampleHidden(initialVisible);
+            return SampleVisible(hidden);
+        }*/
 
         /// <summary>
         /// Given a training example (a vector of visible units), compute the probabilities of the hidden units being activated. 
         /// This involves calculating the conditional probabilities of the hidden units given the visible units using the current model parameters.
         /// </summary>
         /// <returns></returns>
-        public NMatrix SampleVisible(NVector input)
+        public NVector SampleHidden(NVector input)
         {
+            NVector result = new NVector(_weights.GetLength(0));
+            // for each hidden neuron
+            for(int i = 0; i < _weights.GetLength(0); ++i)
+            {
+                double activation = _hiddenBias[i];
 
+                for (int j= 0; j < _weights.GetLength(1); ++j)
+                {
+                    activation += _weights[i, j] * input[i];
+                }
+
+                double fire_probability = MLActivationFunctions.Sigmoid(activation);
+
+                result[i] = _random.NextDouble() < fire_probability ? 1 : 0;
+            }
+
+            return result;
         }
 
+        public NVector SampleVisible(NVector input)
+        {
+            var result = new NVector(_weights.GetLength(1));
+            for (int i = 0; i < _weights.GetLength(1); i++) // Loop over columns in the result
+            {
+                double activation = _visibleBias[i];
+
+                for (int j = 0; j < _weights.GetLength(0); j++) // Loop over rows in 'a'
+                {
+                    activation += _weights[j, i] * input[j];
+                }
+                double fire_probability = MLActivationFunctions.Sigmoid(activation);
+                result[i] = _random.NextDouble() < fire_probability ? 1 : 0;
+            }
+            return result;
+        }
 
         public NVector Predict(NVector inputData)
         {
---
+++ Assets/AtomixML/Runtime/UnsupervisedLearning/RestrictedBoltzmanMachine/BooleanRBMTrainer.cs
@@ -0,0 +1,35 @@
+﻿using Atom.MachineLearning.Core;
+using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Atom.MachineLearning.Unsupervised.BoltzmanMachine
+{
+    /// <summary>
+    /// Contrastive divergence / sampling in action
+    /// </summary>
+    public class BooleanRBMTrainer : MonoBehaviour
+    {
+        [Button]
+        private void TestSampleHidden()
+        {
+            var rbm = new BooleanRBMModel(0, "test-brbm-6-2", 6, 2);
+            var input = new NVector(6).Random(0, 1);
+            var sample = rbm.SampleHidden(input);
+            
+            Debug.Log(sample);
+
+            var input2 = rbm.SampleVisible(input);
+            Debug.Log(input2);
+        }
+
+        private void TestSampleVisible()
+        {
+
+        }
+    }
+}
