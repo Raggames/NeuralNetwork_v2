7edf2c5ff3f99069aaabb79c620d88985186abdd
Raggames <enzoboudart@gmail.com>
2024-12-16 23:42:41 +0100

    w

--- Assets/AtomixML/MiniProjects/PIDControllerTuning/PhysicSimulation/SimulatedPIDAgent.cs
+++ Assets/AtomixML/MiniProjects/PIDControllerTuning/PhysicSimulation/SimulatedPIDAgent.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Atom.MachineLearning.MiniProjects.PIDControllerTuning
@@ -7,10 +8,17 @@ namespace Atom.MachineLearning.MiniProjects.PIDControllerTuning
         private Rigidbody _rigidbody;
 
         [SerializeField] private Transform _target;
-        [SerializeField] private PIDFunction _xAxisPID;
-        [SerializeField] private PIDFunction _yAxisPID;
-        [SerializeField] private PIDFunction _zAxisPID;
         [SerializeField] private float _forceMultiplier = 1;
+        [SerializeField] private float _torqueMultiplier = 1;
+
+        [FoldoutGroup("Translation"), SerializeField] private PIDFunction _xAxisPID;
+        [FoldoutGroup("Translation"), SerializeField] private PIDFunction _yAxisPID;
+        [FoldoutGroup("Translation"), SerializeField] private PIDFunction _zAxisPID;
+
+        [FoldoutGroup("Rotation"), SerializeField] private PIDFunction _xRotAxisPID;
+        [FoldoutGroup("Rotation"), SerializeField] private PIDFunction _yRotAxisPID;
+        [FoldoutGroup("Rotation"), SerializeField] private PIDFunction _zRotAxisPID;
+
 
         private void Awake()
         {
@@ -19,6 +27,9 @@ namespace Atom.MachineLearning.MiniProjects.PIDControllerTuning
             _xAxisPID.Initialize(Time.fixedDeltaTime);
             _yAxisPID.Initialize(Time.fixedDeltaTime);
             _zAxisPID.Initialize(Time.fixedDeltaTime);
+            _xRotAxisPID.Initialize(Time.fixedDeltaTime);
+            _yRotAxisPID.Initialize(Time.fixedDeltaTime);
+            _zRotAxisPID.Initialize(Time.fixedDeltaTime);
         }
 
         private void FixedUpdate()
@@ -30,6 +41,20 @@ namespace Atom.MachineLearning.MiniProjects.PIDControllerTuning
             var z_force = (float)_yAxisPID.Compute(offset.z, 0);
 
             _rigidbody.AddForce(new Vector3(x_force, y_force, z_force) * _forceMultiplier);
+
+            var offsetR = transform.rotation.eulerAngles - _target.rotation.eulerAngles;
+
+            var xr_force = (float)_xRotAxisPID.Compute(WrapAngle(offsetR.x), 0);
+            var yr_force = (float)_yRotAxisPID.Compute(WrapAngle(offsetR.y), 0);
+            var zr_force = (float)_zRotAxisPID.Compute(WrapAngle(offsetR.z), 0);
+
+            //_rigidbody.AddForceAtPosition(Vector3.right, new Vector3(0, zr_force, 0) * _torqueMultiplier);
+            _rigidbody.AddTorque(new Vector3(xr_force, yr_force,  zr_force) * _torqueMultiplier);
+        }
+
+        private float WrapAngle(float angle)
+        {
+            return angle > 180 ? angle - 360 : angle;
         }
     }
 
